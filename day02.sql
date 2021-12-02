d1011698ff8639c9c4b2c2da79a54d508f024e60
xrestorff <xrestorff@gmail.com>
2021-12-02 19:20:06 +0100

    second part

--- day02.sql
+++ day02.sql
@@ -12,3 +12,30 @@ with split_data as (
      )
 select forward * (down - up)
 from command_sums;
+
+-- part two
+with base_data as (
+    select substr(command, 1, instr(command, ' ') - 1) as command,
+           rightstr(command, 1)                        as value,
+           row_number() over ()                        as row_num
+    from day02_data
+),
+     add_sums as (
+         select *,
+                case when command = 'forward' then value else 0 end as forward,
+                case when command = 'down' then value else 0 end    as down,
+                case when command = 'up' then value else 0 end      as up
+         from base_data
+     ),
+     add_aim as (
+         select *,
+                sum(down) over (order by row_num) - sum(up) over (order by row_num) as aim
+         from add_sums
+     ),
+     add_depth as (
+         select *,
+                aim * forward as depth
+         from add_aim
+     )
+select sum(forward) * sum(depth)
+from add_depth
