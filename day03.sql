914355190caa7585245bb20e9cd7423dce816286
xrestorff <xrestorff@gmail.com>
2021-12-03 20:35:55 +0100

    finish day 3

--- day03.sql
+++ day03.sql
@@ -39,3 +39,74 @@ with recursive
     )
 select sum(gamma_dec) * sum(epsilon_dec)
 from add_decimals;
+
+-- part two
+with recursive
+    oxygen as (
+        select input, counter, count(*) over () as row_count
+        from (
+                 select input,
+                        1                                    as counter,
+                        cast(substr(input, 1, 1) as integer) as current_bit,
+                        sum(substr(input, 1, 1)::integer) over () as bit_sum,
+                        count(*) over ()                          as num_rows
+                 from day03
+             ) as base
+        where current_bit = case
+           when bit_sum >= (num_rows - bit_sum) then 1
+           else 0 end
+        union all
+        select input, counter, count(*) over ()
+        from (
+                 select input,
+                        counter + 1                                    as counter,
+                        cast(substr(input, counter + 1, 1) as integer) as current_bit,
+                        sum(substr(input, counter + 1, 1)::integer) over () as bit_sum,
+                        count(*) over ()                          as num_rows
+                 from oxygen
+                 where row_count > 1
+             ) as base
+        where current_bit = case
+           when bit_sum >= (num_rows - bit_sum) then 1
+           else 0 end
+    ),
+
+    co2 as (
+        select input, counter, count(*) over () as row_count
+        from (
+                 select input,
+                        1                                    as counter,
+                        cast(substr(input, 1, 1) as integer) as current_bit,
+                        sum(substr(input, 1, 1)::integer) over () as bit_sum,
+                        count(*) over ()                          as num_rows
+                 from day03
+             ) as base
+        where current_bit = case
+           when bit_sum >= (num_rows - bit_sum) then 0
+           else 1 end
+        union all
+        select input, counter, count(*) over ()
+        from (
+                 select input,
+                        counter + 1                                    as counter,
+                        cast(substr(input, counter + 1, 1) as integer) as current_bit,
+                        sum(substr(input, counter + 1, 1)::integer) over () as bit_sum,
+                        count(*) over ()                          as num_rows
+                 from co2
+                 where row_count > 1
+             ) as base
+        where current_bit = case
+           when bit_sum >= (num_rows - bit_sum) then 0
+           else 1 end
+    ),
+
+    oxygen_binary as (
+        select distinct last_value(input) over () as oxygen
+        from oxygen),
+    co2_binary as (
+        select distinct last_value(input) over () as co2
+        from co2)
+
+select (co2_binary.co2::bit(12))::integer * (oxygen_binary.oxygen::bit(12))::integer
+from oxygen_binary
+         cross join co2_binary;
