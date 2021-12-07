b7907686ee8a0ec66287173a1fe5227d314cd670
xrestorff <xrestorff@gmail.com>
2021-12-07 17:38:38 +0100

    day 5 part 2

--- day05.sql
+++ day05.sql
@@ -74,3 +74,64 @@ with recursive
     )
 select count(*)
 from multiple_points;
+
+-- part two
+with recursive
+    parsed as (
+        select *,
+               substr(input, 1, position(',' in input) - 1)::int                                                  as x1,
+               substr(substr(input, position(',' in input) + 1), 1,
+                      position(' ' in substr(input, position(',' in input) + 1)))::int                            as y1,
+               reverse(substr(reverse(input), 1, position(',' in reverse(input)) - 1))::int                       as y2,
+               reverse(substr(substr(reverse(input), position(',' in reverse(input)) + 1), 1,
+                              position(' ' in substr(reverse(input), position(',' in reverse(input)) + 1))))::int as x2
+        from day05
+    ),
+    coords as (
+        select *,
+               x1 = x2                  as is_vertical,
+               y1 = y2                  as is_horizontal,
+               not (x1 = x2 or y1 = y2) as is_diagonal,
+               x2 > x1                  as is_x_asc,
+               y2 > y1                  as is_y_asc
+        from parsed
+    ),
+    distances as (
+        select *,
+               case
+                   when is_vertical then abs(y2 - y1)
+                   else abs(x2 - x1)
+                   end as distance
+        from coords
+    ),
+    steps as (
+        select 0 as step
+        union all
+        select step + 1
+        from steps
+        where step < (select max(distance) from distances)
+    ),
+    covered_points as (
+        select *,
+               case
+                   when is_vertical then x1
+                   when is_x_asc then x1 + steps.step
+                   else x1 - steps.step
+                   end as x,
+               case
+                   when is_horizontal then y1
+                   when is_y_asc then y1 + steps.step
+                   else y1 - steps.step
+                   end as y
+        from distances
+                 inner join steps
+                            on distances.distance >= steps.step
+    ),
+    multiple_points as (
+        select x, y, count(*)
+        from covered_points
+        group by 1, 2
+        having count(*) > 1
+    )
+select count(*)
+from multiple_points;
