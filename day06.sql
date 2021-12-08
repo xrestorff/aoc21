c6a5f3d9929f5d4c6dca8e351a4ae6b8ab26887a
xrestorff <xrestorff@gmail.com>
2021-12-08 06:48:58 +0100

    day 6 part 2

--- day06.sql
+++ day06.sql
@@ -27,3 +27,38 @@ with recursive fish as (
 select count(*)
 from fish
 where counter = 80;
+
+-- part two
+with recursive fish as (
+    select 0                as counter,
+           input,
+           count(*)::bigint as amount
+    from day06
+    group by 1, 2
+    union all
+    select *
+    from (
+             with fish_inner as (select * from fish)
+             select counter + 1,
+                    input,
+                    sum(amount)::bigint
+             from (
+                      select counter,
+                             case
+                                 when input = 0 then 6
+                                 else input - 1
+                                 end as input,
+                             amount
+                      from fish_inner
+                      union all
+                      select counter, 8, amount
+                      from fish_inner
+                      where input = 0
+                  ) as base
+             where counter < 256
+             group by 1, 2
+         ) as pg_workaround
+)
+select sum(amount)
+from fish
+where counter = 256;
