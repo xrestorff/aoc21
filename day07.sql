a29e8763cf0803ed67d6f1e961e1a95fd6ab35c1
xrestorff <xrestorff@gmail.com>
2021-12-08 08:22:49 +0100

    positions are contiguuous

--- day07.sql
+++ day07.sql
@@ -1,16 +1,20 @@
 -- part one
-with positions as (
-    select distinct input as position
-    from day07
-),
-     distances as (
-         select *, abs(day07.input - positions.position) as distance
-         from day07
-                  cross join positions),
-     sum_distances as (
-         select position, sum(distance) as distance
-         from distances
-         group by 1
-     )
+with recursive
+    positions as (
+        select 0 as position
+        union all
+        select position + 1
+        from positions
+        where position < (select max(input) from day07)
+    ),
+    distances as (
+        select *, abs(day07.input - positions.position) as distance
+        from day07
+                 cross join positions),
+    sum_distances as (
+        select position, sum(distance) as distance
+        from distances
+        group by 1
+    )
 select min(distance)
 from sum_distances;
