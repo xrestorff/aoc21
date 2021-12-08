167eb3e6dac7e7242af81581472bd706e76ff68c
xrestorff <xrestorff@gmail.com>
2021-12-08 08:25:53 +0100

    day 7 part 2

--- day07.sql
+++ day07.sql
@@ -18,3 +18,27 @@ with recursive
     )
 select min(distance)
 from sum_distances;
+
+-- part two
+with recursive
+    positions as (
+        select 0 as position
+        union all
+        select position + 1
+        from positions
+        where position < (select max(input) from day07)
+    ),
+    distances as (
+        select *, abs(day07.input - positions.position) as distance
+        from day07
+                 cross join positions),
+    sum_distances as (
+        select position,
+               sum(
+                       distance * (distance + 1) / 2
+                   ) as distance
+        from distances
+        group by 1
+    )
+select min(distance)
+from sum_distances;
